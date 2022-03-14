598511ec1342f70d9c139ec2700a11c99b98c700
Fellipe Vasconcelos <fsilva@appen.com>
2022-03-14 15:12:58 -0300

    first commit

---
+++ precache-manifest.f1fc4e1b4e3d37929cedab257a358ee6.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf181c4ede1a09f1deaecb2acabee2a",
+    "url": "/qrp/v2/services/project-setup/index.html"
+  },
+  {
+    "revision": "26f78e9d306ac9f9c573",
+    "url": "/qrp/v2/services/project-setup/static/css/2.77bc1963.chunk.css"
+  },
+  {
+    "revision": "f1f1dc4c4c4f1e363e4d",
+    "url": "/qrp/v2/services/project-setup/static/css/main.70f09c43.chunk.css"
+  },
+  {
+    "revision": "26f78e9d306ac9f9c573",
+    "url": "/qrp/v2/services/project-setup/static/js/2.ac6a323b.chunk.js"
+  },
+  {
+    "revision": "8484feb36c0f8c072c9fd909883787f9",
+    "url": "/qrp/v2/services/project-setup/static/js/2.ac6a323b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f1dc4c4c4f1e363e4d",
+    "url": "/qrp/v2/services/project-setup/static/js/main.607c498d.chunk.js"
+  },
+  {
+    "revision": "07d6eb34b165002cde69",
+    "url": "/qrp/v2/services/project-setup/static/js/runtime-main.9137f476.js"
+  },
+  {
+    "revision": "a4ad51040fb356d2159d6ace93115146",
+    "url": "/qrp/v2/services/project-setup/static/media/AppenBasis-Bold.a4ad5104.otf"
+  },
+  {
+    "revision": "71f0255fedd15c49321cb2de1ad14e29",
+    "url": "/qrp/v2/services/project-setup/static/media/AppenBasis-BoldItalic.71f0255f.otf"
+  },
+  {
+    "revision": "72cdc8909485f7d219533a8ad3d32c0a",
+    "url": "/qrp/v2/services/project-setup/static/media/AppenBasis-Italic.72cdc890.otf"
+  },
+  {
+    "revision": "abde3b0f74dc2e7fe634e54df11f9d9e",
+    "url": "/qrp/v2/services/project-setup/static/media/AppenBasis-Medium.abde3b0f.otf"
+  },
+  {
+    "revision": "ddc84e17fb58f5c2f6d4b94bdd1619e9",
+    "url": "/qrp/v2/services/project-setup/static/media/AppenBasis-MediumItalic.ddc84e17.otf"
+  },
+  {
+    "revision": "cb907a8a255cb4852333e6929040c0d8",
+    "url": "/qrp/v2/services/project-setup/static/media/AppenBasis-Regular.cb907a8a.otf"
+  },
+  {
+    "revision": "ce084623e94eb6380c4ffb5237173fdf",
+    "url": "/qrp/v2/services/project-setup/static/media/academy.ce084623.svg"
+  },
+  {
+    "revision": "ab3aed46f639a57e0715c507ceac6850",
+    "url": "/qrp/v2/services/project-setup/static/media/add-new-rounded.ab3aed46.svg"
+  },
+  {
+    "revision": "88cf370c6ecd87f892b8eefec63da27e",
+    "url": "/qrp/v2/services/project-setup/static/media/add.88cf370c.svg"
+  },
+  {
+    "revision": "089edf5e920dcb55fc335f1c72081105",
+    "url": "/qrp/v2/services/project-setup/static/media/arrow-right.089edf5e.svg"
+  },
+  {
+    "revision": "a4f3863f5a58eb38373ac6322d3d2280",
+    "url": "/qrp/v2/services/project-setup/static/media/attention-grey.a4f3863f.svg"
+  },
+  {
+    "revision": "f33ca24351fdb856cbcbd23af3b8da17",
+    "url": "/qrp/v2/services/project-setup/static/media/attention-red.f33ca243.svg"
+  },
+  {
+    "revision": "bd6c67ec37221d3ec8e7682c438b0e5a",
+    "url": "/qrp/v2/services/project-setup/static/media/calendar.bd6c67ec.svg"
+  },
+  {
+    "revision": "d59879e5655765b9a2c6392c2bfe2dbc",
+    "url": "/qrp/v2/services/project-setup/static/media/certificate.d59879e5.svg"
+  },
+  {
+    "revision": "2ddbd599e6c602241d3d7f76953ab7e8",
+    "url": "/qrp/v2/services/project-setup/static/media/chart.2ddbd599.svg"
+  },
+  {
+    "revision": "e98fb241271701f2583ffdd5b2114dbc",
+    "url": "/qrp/v2/services/project-setup/static/media/check.e98fb241.svg"
+  },
+  {
+    "revision": "6085eedddd7ecbeb82874b5b3aa5ba96",
+    "url": "/qrp/v2/services/project-setup/static/media/checkbox-group.6085eedd.svg"
+  },
+  {
+    "revision": "bb48b98ceeda84f42bbca47ec5062b67",
+    "url": "/qrp/v2/services/project-setup/static/media/checkmark.bb48b98c.svg"
+  },
+  {
+    "revision": "268056316ee271e12dd98387f9a15025",
+    "url": "/qrp/v2/services/project-setup/static/media/chevron-down-blue.26805631.svg"
+  },
+  {
+    "revision": "cf181f4885216eddb5e82a3538385da6",
+    "url": "/qrp/v2/services/project-setup/static/media/chevron-down-gray.cf181f48.svg"
+  },
+  {
+    "revision": "4569fb43b4a271037f4108ec1ffd6f02",
+    "url": "/qrp/v2/services/project-setup/static/media/chevron-down.4569fb43.svg"
+  },
+  {
+    "revision": "383ffe0a575d8caa1ad0b5678167323f",
+    "url": "/qrp/v2/services/project-setup/static/media/chevron-left.383ffe0a.svg"
+  },
+  {
+    "revision": "1979234bf6034407edc486ca0c1931b0",
+    "url": "/qrp/v2/services/project-setup/static/media/chevron-right.1979234b.svg"
+  },
+  {
+    "revision": "005f3e50b49466c73f2bd7c2b6ca7ee4",
+    "url": "/qrp/v2/services/project-setup/static/media/circle-check.005f3e50.svg"
+  },
+  {
+    "revision": "be04e8fb0811fdd27c60023abb9570b5",
+    "url": "/qrp/v2/services/project-setup/static/media/close-blue.be04e8fb.svg"
+  },
+  {
+    "revision": "d84869dd1e6d5ed832a8dda2cef3ba7d",
+    "url": "/qrp/v2/services/project-setup/static/media/close-circle.d84869dd.svg"
+  },
+  {
+    "revision": "682fef6886297a6b446ba6146e82a038",
+    "url": "/qrp/v2/services/project-setup/static/media/close-green.682fef68.svg"
+  },
+  {
+    "revision": "f1f65aa5690b1530244202644ae3629c",
+    "url": "/qrp/v2/services/project-setup/static/media/close-input.f1f65aa5.svg"
+  },
+  {
+    "revision": "558e2e419cd5abb5ed16777ff2df3df5",
+    "url": "/qrp/v2/services/project-setup/static/media/close-red.558e2e41.svg"
+  },
+  {
+    "revision": "801c37bcedb47aba6bd70e4be33d51c7",
+    "url": "/qrp/v2/services/project-setup/static/media/close.801c37bc.svg"
+  },
+  {
+    "revision": "94f990288678e5bb807c20cb1cbfdacc",
+    "url": "/qrp/v2/services/project-setup/static/media/copy-goal.94f99028.svg"
+  },
+  {
+    "revision": "59e1f5dd0bc66d4a98abaeb8502719cf",
+    "url": "/qrp/v2/services/project-setup/static/media/copy.59e1f5dd.svg"
+  },
+  {
+    "revision": "d1d244037afc9422a3ea5d8b33e61d25",
+    "url": "/qrp/v2/services/project-setup/static/media/datepicker.d1d24403.svg"
+  },
+  {
+    "revision": "513402660ee59f517ab3282b054dfcba",
+    "url": "/qrp/v2/services/project-setup/static/media/document.51340266.svg"
+  },
+  {
+    "revision": "e61cea6c55269f1c9d7f48ade5afd1dc",
+    "url": "/qrp/v2/services/project-setup/static/media/double-chevron-right.e61cea6c.svg"
+  },
+  {
+    "revision": "00c5250a8f3240fdf14dcae840b09e60",
+    "url": "/qrp/v2/services/project-setup/static/media/dropdown.00c5250a.svg"
+  },
+  {
+    "revision": "571b0bef9bc3be0cf25989178c240676",
+    "url": "/qrp/v2/services/project-setup/static/media/elipsis.571b0bef.svg"
+  },
+  {
+    "revision": "0c104e3157b72bc65ec8856bf099cae7",
+    "url": "/qrp/v2/services/project-setup/static/media/express.0c104e31.svg"
+  },
+  {
+    "revision": "db8f6421ac135b4890b0a8834f9759a6",
+    "url": "/qrp/v2/services/project-setup/static/media/eye.db8f6421.svg"
+  },
+  {
+    "revision": "733171911a3ec1b6ce1b52cfc79daf38",
+    "url": "/qrp/v2/services/project-setup/static/media/filter.73317191.svg"
+  },
+  {
+    "revision": "6fc45f3a9b8669239ac27dfb29007a2f",
+    "url": "/qrp/v2/services/project-setup/static/media/history-clock.6fc45f3a.svg"
+  },
+  {
+    "revision": "419fed450307ab9a217b41c281bdb240",
+    "url": "/qrp/v2/services/project-setup/static/media/icon-right-arrow.419fed45.svg"
+  },
+  {
+    "revision": "1d75174799ee6790a0f1c4815242b8a1",
+    "url": "/qrp/v2/services/project-setup/static/media/linguistics.1d751747.svg"
+  },
+  {
+    "revision": "a765e341d4d60e884f3419b6ee566c07",
+    "url": "/qrp/v2/services/project-setup/static/media/mail.a765e341.svg"
+  },
+  {
+    "revision": "641c9b691e5e1d73dda7ea7c8d32b29f",
+    "url": "/qrp/v2/services/project-setup/static/media/pause.641c9b69.svg"
+  },
+  {
+    "revision": "7586c7ef844d8065b2932d9a2ae3e338",
+    "url": "/qrp/v2/services/project-setup/static/media/pencil-blue.7586c7ef.svg"
+  },
+  {
+    "revision": "972bed1038a71a0cd975b9bebcf12c5b",
+    "url": "/qrp/v2/services/project-setup/static/media/pencil.972bed10.svg"
+  },
+  {
+    "revision": "bba8066fb5ee153fdfbfa469ebf5db64",
+    "url": "/qrp/v2/services/project-setup/static/media/project-completed.bba8066f.svg"
+  },
+  {
+    "revision": "0888b4e45000dafd1f16b8cb99b5e91e",
+    "url": "/qrp/v2/services/project-setup/static/media/quiz.0888b4e4.svg"
+  },
+  {
+    "revision": "4264ff85d7587ebf82db54413f19643d",
+    "url": "/qrp/v2/services/project-setup/static/media/radio.4264ff85.svg"
+  },
+  {
+    "revision": "41386eb5d2a6489aa5bbaf083c914b5b",
+    "url": "/qrp/v2/services/project-setup/static/media/search-evaluation.41386eb5.svg"
+  },
+  {
+    "revision": "31497fe180968bf23ce3b32ea0ffa95b",
+    "url": "/qrp/v2/services/project-setup/static/media/search.31497fe1.svg"
+  },
+  {
+    "revision": "f7922413b050ce6f5130e1622fe3ba4c",
+    "url": "/qrp/v2/services/project-setup/static/media/settings-grey.f7922413.svg"
+  },
+  {
+    "revision": "dc203b6b8ff2bc31337ed4dce802caed",
+    "url": "/qrp/v2/services/project-setup/static/media/small-check.dc203b6b.svg"
+  },
+  {
+    "revision": "ffd8e02848349232ed42400dc4b42b79",
+    "url": "/qrp/v2/services/project-setup/static/media/small-logo.ffd8e028.svg"
+  },
+  {
+    "revision": "6670cdb5e3ae4b9414285649d9eb800e",
+    "url": "/qrp/v2/services/project-setup/static/media/social-media.6670cdb5.svg"
+  },
+  {
+    "revision": "46178cfed8fc3d75c929a69c85058a58",
+    "url": "/qrp/v2/services/project-setup/static/media/star.46178cfe.svg"
+  },
+  {
+    "revision": "6c48cbaa3ee10268a3b2e5e176785312",
+    "url": "/qrp/v2/services/project-setup/static/media/survey.6c48cbaa.svg"
+  },
+  {
+    "revision": "5096272d12f9a1755569c96bc997f5da",
+    "url": "/qrp/v2/services/project-setup/static/media/text.5096272d.svg"
+  },
+  {
+    "revision": "0d1954d5d52a6b7229731de8adc9a09e",
+    "url": "/qrp/v2/services/project-setup/static/media/transcription.0d1954d5.svg"
+  },
+  {
+    "revision": "3f4d84d0674794839120304df71ebe84",
+    "url": "/qrp/v2/services/project-setup/static/media/translation.3f4d84d0.svg"
+  },
+  {
+    "revision": "64df1170298bd16780c5eded56d78d4b",
+    "url": "/qrp/v2/services/project-setup/static/media/user-project-page.64df1170.svg"
+  },
+  {
+    "revision": "55153999fe2c030b990bbde27d9ad8af",
+    "url": "/qrp/v2/services/project-setup/static/media/web-research.55153999.svg"
+  }
+]);
